19ff552bf0b374f9c434a8ede193085cc01e535c
Sirisha <sirisha20.vr@gmail.com>
2022-05-01 16:04:54 -0400

    JIRA#Update to the message

--- Mproject/src/test/java/Ppackage/DemoTwoTest.java
+++ Mproject/src/test/java/Ppackage/DemoTwoTest.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class DemoTwoTest {
 	@Test
 	public void mTwo() {
-		System.out.println("Inside mTwo method from DemoTwoTest");
+		System.out.println("Inside mTwo method from DemoTwoTest ....");
 	}
 
 }
